6526ca8389dc11c6628e0b7d1f3fba73528f58b7
easonchan17 <126367585+easonchan17@users.noreply.github.com>
2023-04-12 14:28:36 +0800

    fix: addMinerPower refuse to write power for the rounds that have already been frozen (#13)

--- contracts/interface/ICandidateHub.sol
+++ contracts/interface/ICandidateHub.sol
@@ -4,4 +4,5 @@ pragma solidity 0.8.4;
 interface ICandidateHub {
   function canDelegate(address agent) external view returns(bool);
   function jailValidator(address operateAddress, uint256 round, uint256 fine) external;
+  function getRoundTag() external view returns(uint256);
 }
