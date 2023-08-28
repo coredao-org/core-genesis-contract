288fa2a590b92db42ebabd8b744c6d018475623d
zfliex <zfliex924@gmail.com>
2023-08-28 18:14:33 +0800

    fix: audit modification opinion

--- contracts/interface/IPledgeAgent.sol
+++ contracts/interface/IPledgeAgent.sol
@@ -6,4 +6,5 @@ interface IPledgeAgent {
   function getHybridScore(address[] calldata candidates, uint256[] calldata powers) external returns(uint256[] memory, uint256, uint256);
   function setNewRound(address[] calldata validatorList, uint256 totalPower, uint256 totalCoin, uint256 round) external;
   function distributePowerReward(address candidate, address[] calldata miners) external;
+  function onFelony(address agent) external;
 }
