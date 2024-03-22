b582f0d4b94ce9f6baa91283ccda6274a95952de
zfliex <zfliex924@gmail.com>
2024-03-22 11:54:14 +0800

    fix: audit modification opinion

--- contracts/interface/IPledgeAgent.sol
+++ contracts/interface/IPledgeAgent.sol
@@ -7,4 +7,13 @@ interface IPledgeAgent {
   function setNewRound(address[] calldata validatorList, uint256 round) external;
   function distributePowerReward(address candidate, address[] calldata miners) external;
   function onFelony(address agent) external;
+  function delegateCoin(address agent) external payable;
+  function undelegateCoin(address agent) external;
+  function undelegateCoin(address agent, uint256 amount) external;
+  function transferCoin(address sourceAgent, address targetAgent) external;
+  function transferCoin(address sourceAgent, address targetAgent, uint256 amount) external;
+  function claimReward(address[] calldata agentList) external returns (uint256, bool);
+  function delegateBtc(bytes calldata btcTx, uint32 blockHeight, bytes32[] memory nodes, uint256 index, bytes memory script) external;
+  function transferBtc(bytes32 txid, address targetAgent) external;
+  function claimBtcReward(bytes32[] calldata txidList) external returns (uint256 rewardSum);
 }
