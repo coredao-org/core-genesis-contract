74142d4bbafeb0c1d2cb6e4338b69652ce2ec357
barneychambers <barneychambers@hotmail.com>
2024-06-24 13:18:42 +0100

    data dump, do not merge

--- contracts/BitcoinLSTStake.sol
+++ contracts/BitcoinLSTStake.sol
@@ -13,6 +13,66 @@ import "./lib/BitcoinHelper.sol";
 import "./System.sol";
 
 contract BitcoinLSTStake is IBitcoinLSTStake, System, IParamSubscriber {
+  
+  
+  /* Barney and Oscar's Code */
+
+  uint256 public constant ROUND_INTERVAL = 86400;
+  uint256 public constant BTC_DECIMAL = 1e8;
+
+  uint256 public lastRewardDistribution;
+  IPledgeAgent public pledgeAgent;
+
+  event distributedReward(uint256 reward, uint256 roundTag);
+
+  constructor(address _pledgeAgent) {
+    pledgeAgent = IPledgeAgent(_pledgeAgent);
+    lastRewardDistribution = block.timestamp;
+  }
+
+  modifier onlyPldegeAgent() {
+    require(msg.sender == address(pledgeAgent), "only pledge agent can call this function");
+    _;
+  }
+
+  // implement a way to talk to the consensus engine, and figure out who is staking btc to get LST tokens
+    // LST contracts are a black box from the perspective of the protocol contracts, so we need to figure out who's LSTing (we dont get told this)
+
+
+  // implement a way for the CORE protocol to call a (payable?) function and send in a bunch of Core rewards (probably at the end of each round)
+
+  // implement a function for the LST protocol that distributes the rewards based 
+
+
+  /* end Barney and Oscar's code */
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   uint256 public constant ST_ACTIVE = 1;
   uint256 public constant ST_INACTIVE = 2;
 
@@ -31,18 +91,28 @@ contract BitcoinLSTStake is IBitcoinLSTStake, System, IParamSubscriber {
 
   function parsePayload(bytes29 payload) internal pure returns (address delegator) {
     require(payload.len() >= 27, "payload length is too small");
+    //get the CORE evm address that the user chose to get the lst tokens sent to, out of the bitcoin transaction
     delegator = payload.indexAddress(7);
   }
 
+
+/*
+Function for the delegator or the relayer to tell the LST contract they've deposited BTC, and then the delegator gets LST tokens
+*/
   function delegate(bytes memory payload, bytes memory script, uint256 value) external onlyBtcAgent {
 
     address delegator = parsePayload(payload);
 
     require(IRelayerHub(RELAYER_HUB_ADDR).isRelayer(msg.sender), "only delegator or relayer can submit the BTC transaction");
 
+    
     lstToken.mint(delegator, value);
   }
 
+
+/*
+Function for the validator to burn the LST token and get the BTC back
+*/
   function undelegate(bytes btctx) external onlyBtcAgent {
     // TODO mark a burn workflow finish
     bytes[] lockscripts;
@@ -55,9 +125,14 @@ contract BitcoinLSTStake is IBitcoinLSTStake, System, IParamSubscriber {
     lstToken.burn(lockscripts, amounts); 
   }
 
-  function distributeReward(uint256 reward, uint256 roundTag) external payable {
+  function distributeReward(uint256 reward, uint256 roundTag) external payable onlyPldegeAgent {
+    require(block.timestamp - lastRewardDistribution >= ROUND_INTERVAL, "reward distribution interval too short");
+    lastRewardDistribution = block.timestamp;
+    
     rewardPerBTC[roundTag] += rewardPerBTC[lastRoundTag] + reward * BTC_DECIMAL / totalAmount;
     lastRoundTag = roundTag;
+
+    emit distributedReward(reward, roundTag);
   }
 
   /// Get stake amount
