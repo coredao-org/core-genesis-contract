1265db3eb0cbb60f57f4ab3cb7caf10272d7bd58
barneychambers <barneychambers@hotmail.com>
2024-06-25 12:03:36 +0100

    rough design, unmerklized rewards... [TODO: merklize distribute reward and claim reward functionality ]

--- contracts/BitcoinLSTStake.sol
+++ contracts/BitcoinLSTStake.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache2.0
 pragma solidity 0.8.4;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./interface/IAgent.sol";
 import "./interface/IParamSubscriber.sol";
 import "./interface/ILightClient.sol";
@@ -14,80 +15,104 @@ import "./System.sol";
 
 contract BitcoinLSTStake is IBitcoinLSTStake, System, IParamSubscriber {
   
-  
-  /* Barney and Oscar's Code */
-
-  uint256 public constant ROUND_INTERVAL = 86400;
-  uint256 public constant BTC_DECIMAL = 1e8;
-
-  uint256 public lastRewardDistribution;
-  IPledgeAgent public pledgeAgent;
-
-  event distributedReward(uint256 reward, uint256 roundTag);
-
-  constructor(address _pledgeAgent) {
-    pledgeAgent = IPledgeAgent(_pledgeAgent);
-    lastRewardDistribution = block.timestamp;
-  }
-
-  modifier onlyPldegeAgent() {
-    require(msg.sender == address(pledgeAgent), "only pledge agent can call this function");
-    _;
-  }
-
-  // implement a way to talk to the consensus engine, and figure out who is staking btc to get LST tokens
-    // LST contracts are a black box from the perspective of the protocol contracts, so we need to figure out who's LSTing (we dont get told this)
-
-
-  // implement a way for the CORE protocol to call a (payable?) function and send in a bunch of Core rewards (probably at the end of each round)
-
-  // implement a function for the LST protocol that distributes the rewards based 
-
-
-  /* end Barney and Oscar's code */
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
   uint256 public constant ST_ACTIVE = 1;
   uint256 public constant ST_INACTIVE = 2;
 
-  address public lstToken;
+  IERC20 public lstToken;
   bytes32[] public wallets;
   mapping(byte32 => uint256) walletStatus;
 
   // key: roundtag
   // value: reward per BTC accumulated
-  mapping(uint256 => uint256) rewardPerBTC;
+  mapping(uint256 => uint256) public cumulativeRewardPerBTC;
 
   // delegated value in the current round.
   uint256 public totalAmount;
 
+  uint256 public constant ROUND_INTERVAL = 86400;
+  uint256 public constant BTC_DECIMAL = 1e8;
+
+  uint256 public lastRewardDistributionTimestamp;
+  IPledgeAgent public pledgeAgent;
+
+  event RewardDistributed(uint256 reward, uint256 roundTag);
+  event RewardClaimed(address indexed user, uint256 rewardAmount);
+
+
+  //this may be merkleized for reward claiming, so we can have scalability
+  struct UserStakeInfo {
+    uint256 stakedAmount; // Amount of BTC staked
+    uint256 rewardDebt; // Accumulated reward per BTC
+  }
+
+  mapping(address => UserStakeInfo) public userStakeInfo;
+
+  /* These value will need to be converted to merkle tree */
+  mapping(address => bool) public holders;
+  address[] public holderAddresses;
+  /* </end merkle tree values >*/
+
+
+  uint256 public totalBTCStaked;
   uint256 public lastRoundTag;
+
+  constructor(address _pledgeAgent, IERC20 _lstToken) {
+    pledgeAgent = IPledgeAgent(_pledgeAgent);
+    lastRewardDistributionTimestamp = block.timestamp;
+    lstToken = _lstToken;
+  }
+
+  modifier onlyPledgeAgent() {
+    require(msg.sender == address(pledgeAgent), "only pledge agent can call this function");
+    _;
+  }
+
+  function claimReward() external {
+      require(cumulativeRewardPerBTC.length > lastRoundTag , "No cumulativeRewardPerBTC for lastRoundTag");
+      UserStakeInfo storage user = userStakeInfo[msg.sender];
+      
+      // Calculate the pending reward for the user up to the latest round
+      uint256 pendingReward = (user.stakedAmount * cumulativeRewardPerBTC[lastRoundTag]) / BTC_DECIMAL - user.rewardDebt;
+      
+      // If there's a pending reward, transfer it to the user
+      if (pendingReward > 0) {
+          Address.sendValue(payable(msg.sender), pendingReward);
+          emit RewardClaimed(msg.sender, pendingReward);
+      }
+      
+      // Update the rewardDebt to reflect the rewards claimed up to the latest round
+      user.rewardDebt = (user.stakedAmount * cumulativeRewardPerBTC[lastRoundTag]) / BTC_DECIMAL;
+  }
+
+  function distributeReward(uint256 roundTag) external payable onlyPledgeAgent {
+      require(block.timestamp - lastRewardDistributionTimestamp >= ROUND_INTERVAL, "Reward distribution interval too short");
+      require(msg.value > 0, "Reward amount must be greater than 0");
+      require(totalBTCStaked > 0, "No BTC staked");
+      
+      lastRewardDistributionTimestamp = block.timestamp;
+      uint256 rewardAmount = msg.value;
+
+      // Distribute rewards to all current holders
+      uint256 totalSupply = lstToken.totalSupply();
+      require(totalSupply > 0, "Total supply must be greater than 0");
+
+      for (uint256 i = 0; i < holderAddresses.length; i++) {
+          address holder = holderAddresses[i];
+          uint256 holderBalance = lstToken.balanceOf(holder);
+          if (holderBalance > 0) {
+              uint256 holderReward = (rewardAmount * holderBalance) / totalSupply;
+              if (holderReward > 0) {
+                  Address.sendValue(payable(holder), holderReward);
+                  emit RewardClaimed(holder, holderReward);
+              }
+          }
+      }
+
+      cumulativeRewardPerBTC[roundTag] = cumulativeRewardPerBTC[lastRoundTag] + (rewardAmount * BTC_DECIMAL / totalBTCStaked);
+      lastRoundTag = roundTag;
+
+      emit RewardDistributed(rewardAmount, roundTag);
+  }
 
   function parsePayload(bytes29 payload) internal pure returns (address delegator) {
     require(payload.len() >= 27, "payload length is too small");
@@ -96,43 +121,78 @@ contract BitcoinLSTStake is IBitcoinLSTStake, System, IParamSubscriber {
   }
 
 
-/*
-Function for the delegator or the relayer to tell the LST contract they've deposited BTC, and then the delegator gets LST tokens
-*/
+  /*
+  Function for the delegator or the relayer to tell the LST contract they've deposited BTC, and then the delegator gets LST tokens
+  */
   function delegate(bytes memory payload, bytes memory script, uint256 value) external onlyBtcAgent {
-
     address delegator = parsePayload(payload);
-
     require(IRelayerHub(RELAYER_HUB_ADDR).isRelayer(msg.sender), "only delegator or relayer can submit the BTC transaction");
 
-    
-    lstToken.mint(delegator, value);
-  }
+    _mint(delegator, value);
 
+    UserStakeInfo storage user = userStakeInfo[delegator];
+    uint256 pendingReward = (user.stakedAmount * cumulativeRewardPerBTC[lastRoundTag]) / BTC_DECIMAL - user.rewardDebt;
 
-/*
-Function for the validator to burn the LST token and get the BTC back
-*/
-  function undelegate(bytes btctx) external onlyBtcAgent {
-    // TODO mark a burn workflow finish
-    bytes[] lockscripts;
-    uint256[] amounts;
-    for btctx.vout {
-       lockscripts.push(vout[i].pkscript)
-       amounts.push(vout[i].value)
+    user.stakedAmount += value;
+    user.rewardDebt = (user.stakedAmount * cumulativeRewardPerBTC[lastRoundTag]) / BTC_DECIMAL;
+
+    if (pendingReward > 0) {
+        Address.sendValue(payable(delegator), pendingReward);
+        emit RewardClaimed(delegator, pendingReward);
     }
-    //whos tokens are being burned here? shouldn't the user begin the undelegate process by burning the LST tokens?
-    lstToken.burn(lockscripts, amounts); 
+
+    totalBTCStaked += value;
+
+    // Track holders
+    if (!holders[delegator]) {
+        holders[delegator] = true;
+        holderAddresses.push(delegator);
+    }
   }
 
-  function distributeReward(uint256 reward, uint256 roundTag) external payable onlyPldegeAgent {
-    require(block.timestamp - lastRewardDistribution >= ROUND_INTERVAL, "reward distribution interval too short");
-    lastRewardDistribution = block.timestamp;
-    
-    rewardPerBTC[roundTag] += rewardPerBTC[lastRoundTag] + reward * BTC_DECIMAL / totalAmount;
-    lastRoundTag = roundTag;
+  /*
+  Function for the validator to burn the LST token and get the BTC back
+  */
+  function undelegate(bytes memory btcTransaction) external onlyBtcAgent {
+    // TODO mark a burn workflow finish
+    bytes[] memory lockScripts;
+    uint256[] memory amounts;
+    // Parse btcTransaction to get lockScripts and amounts
+    // Iterate over lockScripts and amounts to burn tokens and update user info
+    // example:
+    // _burn(delegator, amount);
 
-    emit distributedReward(reward, roundTag);
+    // Update holder tracking (assuming the burn updates the total supply correctly)
+    for (uint256 i = 0; i < lockScripts.length; i++) {
+        uint256 amount = amounts[i];
+        address holder = address(uint160(uint256(lockScripts[i]))); //somehow get the eth address from the lockscript:
+        UserStakeInfo storage user = userStakeInfo[holder];
+        uint256 pendingReward = (user.stakedAmount * cumulativeRewardPerBTC[lastRoundTag]) / BTC_DECIMAL - user.rewardDebt;
+        
+        user.stakedAmount -= amount;
+        user.rewardDebt = (user.stakedAmount * cumulativeRewardPerBTC[lastRoundTag]) / BTC_DECIMAL;
+
+        lstToken.burn(holder, amount);
+
+        if (pendingReward > 0) {
+            Address.sendValue(payable(holder), pendingReward);
+            emit RewardClaimed(holder, pendingReward);
+        }
+
+        totalBTCStaked -= amount;
+
+        if (lstToken.balanceOf(holder) == 0) {
+            holders[holder] = false;
+            // Remove from holderAddresses (not efficient, consider using a more efficient structure)
+            for (uint256 j = 0; j < holderAddresses.length; j++) {
+                if (holderAddresses[j] == holder) {
+                    holderAddresses[j] = holderAddresses[holderAddresses.length - 1];
+                    holderAddresses.pop();
+                    break;
+                }
+            }
+        }
+    }
   }
 
   /// Get stake amount
@@ -170,7 +230,7 @@ Function for the validator to burn the LST token and get the BTC back
     // decode address -> bytes32addr, networkId
     // verify bitcoin networkId
     // wallets.push(bytes32addr)
-    // walletStatus[addr] = INACTIVE
+    // walletStatus[addr] = ST_ACTIVE
   }
 
   function removeWallet(bytes memory addr) internal {
--- contracts/BitcoinLSTToken.sol
+++ contracts/BitcoinLSTToken.sol
@@ -7,13 +7,6 @@ import "./interfaces/IBitcoinLSTStake.sol";
 contract BTCLSTToken is ERC20 {
 
     address public bitcoinLSTStake;
-    uint256 public rewardPerTokenStored; // Accumulated reward per token
-    uint256 public lastUpdateTime; // Last time rewards were updated
-    uint256 public lastRewardRoundEndTime; // End time of the last reward round
-
-    mapping(address => uint256) public userRewardPerTokenPaid; // User's reward per token paid
-    mapping(address => uint256) public rewards; // User's accumulated rewards
-    mapping(address => uint256) public lastStakeTime; // Timestamp of the last stake
 
     event RewardsDistributed(address[] accounts, uint256[] amounts);
     
@@ -21,62 +14,14 @@ contract BTCLSTToken is ERC20 {
         bitcoinLSTStake = _bitcoinLSTStake;
     }
 
-    modifier updateReward(address account) {
-        rewardPerTokenStored = rewardPerToken();
-        lastUpdateTime = block.timestamp;
-        if (account != address(0)) {
-            rewards[account] = _calculateEarned(account);
-            userRewardPerTokenPaid[account] = rewardPerTokenStored;
-        }
-        _;
-    }
-
-    function rewardPerToken() public view returns (uint256) {
-        if (totalSupply() == 0) {
-            return rewardPerTokenStored;
-        }
-        return rewardPerTokenStored;
-    }
-
-    function _calculateEarned(address account) internal view returns (uint256) {
-        return (balanceOf(account) * (rewardPerTokenStored - userRewardPerTokenPaid[account]) / 1e18) + rewards[account];
-    }
-
-    function mint(address to, uint256 amount) external updateReward(to) {
+    function mint(address to, uint256 amount) external {
         require(msg.sender == bitcoinLSTStake, "Only BTC Agent can mint");
         _mint(to, amount);
-        lastStakeTime[to] = block.timestamp;
     }
 
     function burn(uint256 amount) external {
-
-        if(lastStakeTime[msg.sender] <= lastRewardRoundEndTime && rewards[msg.sender] > 0) {
-            claimReward();
-        }
-
+        require(msg.sender == bitcoinLSTStake, "Only BTC Agent can mint");
         _burn(msg.sender, amount);
-    }
-
-    function distributeRewards(uint256 reward, uint256 roundTag) external {
-        require(msg.sender == bitcoinLSTStake, "Only BTC Agent can distribute rewards");
-
-        // Calculate new reward per token stored
-        if (totalSupply() > 0) {
-            rewardPerTokenStored += (reward * 1e18 / totalSupply());
-        }
-
-        // Update last update time and last reward round end time
-        lastUpdateTime = block.timestamp;
-        lastRewardRoundEndTime = block.timestamp;
-    }
-
-    function claimReward() external updateReward(msg.sender) {
-        require(lastStakeTime[msg.sender] <= lastRewardRoundEndTime, "Must be staked for at least one full round to claim rewards");
-
-        uint256 reward = rewards[msg.sender];
-        require(reward > 0, "No rewards to claim");
-        rewards[msg.sender] = 0;
-        payable(msg.sender).transfer(reward); // Assuming the contract has ETH balance to pay rewards
     }
 
     receive() external payable {}
