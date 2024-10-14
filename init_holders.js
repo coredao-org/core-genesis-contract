8c4176806d6646c5ce37a33ac3dd067dae3294f7
zfliex <zfliex924@gmail.com>
2024-10-14 14:41:06 +0800

    1.1.7 testnet genesis and abi

--- init_holders.js
+++ init_holders.js
@@ -3,12 +3,12 @@ const init_holders = [
    //
    {
       address: "0xa2d48116761f2af265bed1d9b000ebbcca3f12c4",
-      balance: web3.utils.toBN("100000000000000000000000000").toString("hex")
+      balance: "52b7d2dcc80cd2e4000000"
    },
    //
    {
       address: "0x288244402acfdb405f2a1730d7a41033ac6ac271",
-      balance: web3.utils.toBN("500000000000000000000000000").toString("hex")
+      balance: "19d971e4fe8401e74000000"
    },
 ];
 
