231aa3516b8b10f1dc5bc70ca3c7398bd2658e56
falcon <falcon@coredao.org>
2025-01-14 10:52:09 +0800

    Remove PledgeAgent getExpireInfo()

--- contracts/PledgeAgent.sol
+++ contracts/PledgeAgent.sol
@@ -445,10 +445,6 @@ contract PledgeAgent is IPledgeAgent, System, IParamSubscriber {
     return debtDepositMap[round][delegator];
   }
 
-  function getExpireInfo(uint256 round) external view returns (address[] memory) {
-    return round2expireInfoMap[round].agentAddrList;
-  }
-
   receive() external payable {
     if (msg.value != 0) {
       emit received(msg.sender, msg.value);
